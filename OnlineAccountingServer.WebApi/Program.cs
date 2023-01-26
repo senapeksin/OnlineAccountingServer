000f48d1205e6f03d1f62c65f346c0f890f46e23
Senapeksinn <sena@endcozum.com>
2023-01-26 13:23:50 +0300

    changes

--- OnlineAccountingServer.WebApi/Program.cs
+++ OnlineAccountingServer.WebApi/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.InstallServices(builder.Configuration,typeof(IServiceInstaller).Assembly);
-
+//deneme
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
