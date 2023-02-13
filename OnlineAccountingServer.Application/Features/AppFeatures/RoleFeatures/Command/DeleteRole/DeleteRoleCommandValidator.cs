5d3b2f0ddcc746e0f98e1df0f0c83d949c8b8823
Senapeksinn <sena@endcozum.com>
2023-02-13 16:34:19 +0300

    Validator kontrolleri yazıldı

---
+++ OnlineAccountingServer.Application/Features/AppFeatures/RoleFeatures/Command/DeleteRole/DeleteRoleCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace OnlineAccountingServer.Application.Features.AppFeatures.RoleFeatures.Command.DeleteRole
+{
+    public sealed class DeleteRoleCommandValidator : AbstractValidator<DeleteRoleCommand>
+    {
+        public DeleteRoleCommandValidator()
+        {
+            RuleFor(p => p.Id).NotEmpty().WithMessage("Id bilgisi boş olamaz!");
+            RuleFor(p => p.Id).NotNull().WithMessage("Id bilgisi boş olamaz!!");
+        }
+    }
+}
---
+++ OnlineAccountingServer.Application/Features/AppFeatures/RoleFeatures/Command/UpdateRole/UpdateRoleCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace OnlineAccountingServer.Application.Features.AppFeatures.RoleFeatures.Command.UpdateRole
+{
+    public sealed class UpdateRoleCommandValidator : AbstractValidator<UpdateRoleCommand>
+    {
+        public UpdateRoleCommandValidator()
+        {
+            RuleFor(p => p.Id).NotEmpty().WithMessage("Id bilgisi boş olamaz!");
+            RuleFor(p => p.Id).NotNull().WithMessage("Id bilgisi boş olamaz!!");
+            RuleFor(p => p.Code).NotEmpty().WithMessage("Role kodu boş olamaz!");
+            RuleFor(p => p.Code).NotNull().WithMessage("Role kodu boş olamaz!");
+            RuleFor(p => p.Name).NotEmpty().WithMessage("Role adı boş olamaz!");
+            RuleFor(p => p.Name).NotNull().WithMessage("Role adı boş olamaz!");
+        }
+    }
+}
